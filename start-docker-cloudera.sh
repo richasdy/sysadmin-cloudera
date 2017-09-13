485133bd549dc9d85d63bf9961d94c9cdd50ac5e
donni richasdy <richasdy@richasdys-iMac.local>
2017-09-13 14:31:39 +0700

    adding comment for information

--- start-docker-cloudera.sh
+++ start-docker-cloudera.sh
@@ -3,4 +3,9 @@ docker run \
 --privileged=true \
 -p 8888:8888 \
 -t -i -d \
-cloudera/quickstart /usr/bin/docker-quickstart
+cloudera/quickstart \
+/usr/bin/docker-quickstart
+
+# NOTE
+# cloudera/quickstart -> 4.4GB++
+# /usr/bin/docker-quickstart --> start service script
